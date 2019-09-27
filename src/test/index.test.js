71ead46c52a10f1312128b8facf74106a54ea7ea
senthurathiban <senthurathiban@gmail.com>
2019-09-27 16:45:20 +0530

    removed binding of null issue

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,14 +1,3 @@
-import moreLess from './../';
-let config = {
-    id: 'testbtn',
-    text: " this is a huge text with the updated more description",
-    isMobile:false,
-    shortTo:5,
-    moreText:'Open more',
-    lessText:'Open less',
-    continuous:true
-}
-
 test('Testing for the output',()=>{
     expect('hello packages').toBe('hello packages');
 });
