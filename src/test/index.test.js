4c3245df884b4553c68dc6465a85027bf00baa50
senthurathiban <senthurathiban@gmail.com>
2019-09-27 16:41:11 +0530

    test cases uncommented

--- src/test/index.test.js
+++ src/test/index.test.js
@@ -1,16 +1,16 @@
-// import moreLess from './../';
-// let config = {
-//     id: 'testbtn',
-//     text: " this is a huge text with the updated more description",
-//     isMobile:false,
-//     shortTo:5,
-//     moreText:'Open more',
-//     lessText:'Open less',
-//     continuous:true
-// }
+import moreLess from './../';
+let config = {
+    id: 'testbtn',
+    text: " this is a huge text with the updated more description",
+    isMobile:false,
+    shortTo:5,
+    moreText:'Open more',
+    lessText:'Open less',
+    continuous:true
+}
 
-// test('Testing for the output',()=>{
-//     expect(
-//         moreLess(config)
-//     ).toBe('hello packages');
-// });
+test('Testing for the output',()=>{
+    expect(
+        moreLess(config)
+    ).toBe('hello packages');
+});
